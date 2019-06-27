9038196d64584a7a3f7d5aa32221289b0eac378e
ikawaha <ikawaha@users.noreply.github.com>
2019-06-27 22:51:34 +0900

    Add usage

--- main.go
+++ main.go
@@ -9,21 +9,33 @@ import (
 	"strings"
 )
 
-func main(){
+func usage() string {
+	return "nise is a translator from Japanese to Chinese-like fake (hanamogera) string. see github.com/ikawaha/nise"
+}
+
+func run(args []string) int {
 	var r io.ReadCloser
-	if len(os.Args) >= 2 {
-		r = ioutil.NopCloser(strings.NewReader(os.Args[1]))
-	}else{
+	if len(os.Args) >= 1 {
+		r = ioutil.NopCloser(strings.NewReader(args[0]))
+	} else {
 		r = os.Stdin
 	}
 	defer r.Close()
 	scanner := bufio.NewScanner(r)
-	for i:=0; scanner.Scan();i++ {
-		os.Stdout.WriteString(Filter(scanner.Text()))
-		os.Stdout.WriteString("\n")
+	for i := 0; scanner.Scan(); i++ {
+		fmt.Fprintln(os.Stdout, Filter(scanner.Text()))
 	}
-	if err :=scanner.Err(); err != nil{
+	if err := scanner.Err(); err != nil {
 		fmt.Errorf("%v", err)
+		return 1
 	}
-	return
+	return 0
+}
+
+func main() {
+	if len(os.Args) == 2 && os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Fprintln(os.Stdout, usage())
+		os.Exit(1)
+	}
+	os.Exit(run(os.Args[1:]))
 }
